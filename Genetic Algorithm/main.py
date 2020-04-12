0e3702b44cc97f6e171c5e3d5301ae375c40ab37
NeilSCGH <neil.segard@gmail.com>
2020-04-12 19:54:10 +0200

    maj

--- Genetic Algorithm/main.py
+++ Genetic Algorithm/main.py
@@ -18,7 +18,8 @@ def computeFitness(pop):
     return np.sum(abs(pop-cible),axis=1)
 
 ##Training
-def evolute(population,nbEpoch=1000,mutation=4,elite=75,totalCrossover=False,mutationElite=True):
+def evolute(population,nbEpoch=5000,mutation=0.6,elite=5,floatingCrossover=False,mutationElite=True,verbose=0):
+    print("ep {}, mut {}, el {}, cross {}, mutel {}\n".format(nbEpoch,mutation,elite,floatingCrossover,mutationElite))
     nbPopulation=len(pop)
 
     nbElite=int(nbPopulation*elite/100)
@@ -33,7 +34,7 @@ def evolute(population,nbEpoch=1000,mutation=4,elite=75,totalCrossover=False,mut
         fitness=fitness[trieur]
         population=population[trieur]
 
-        if epoch%(nbEpoch/10)==0:
+        if verbose!=0 and epoch%(nbEpoch/100*verbose)==0:
             pourcent=int(epoch/nbEpoch*100)
             minFitness=int(np.round(fitness[0]))
             maxFitness=int(np.round(fitness[-1]))
@@ -46,7 +47,7 @@ def evolute(population,nbEpoch=1000,mutation=4,elite=75,totalCrossover=False,mut
             parent1=population[np.random.randint(0,nbElite)]
             parent2=population[np.random.randint(0,nbElite)]
 
-            if totalCrossover:#one or the other
+            if floatingCrossover:#one or the other
                 cm=np.random.randint(0,1,size=(1,personSize)) #generating the crossover mask
             else:#a mix of both
                 cm=np.random.uniform(0,1,size=(1,personSize)) #generating the crossover mask
@@ -68,9 +69,8 @@ def evolute(population,nbEpoch=1000,mutation=4,elite=75,totalCrossover=False,mut
     p=population[fitness.argsort()]
     return p[0],p[-1]
 
-#combiner randomly avec un tableau de 1 et de 0, multiplier puis additionner les 2 parties complementaires pour le crossover
 
-best,worst=evolute(pop,10000)
+best,worst=evolute(pop,nbEpoch=5000,mutation=0.6,elite=5,floatingCrossover=True,mutationElite=False,verbose=5)
 #print(best)
-print("fitness:",computeFitness(best)[0])
+print("\nfitness:",computeFitness(best)[0])
 print("fitness:",computeFitness(worst)[0])
