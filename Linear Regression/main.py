a056393fd833cce9524ef702a7846324cf13e04b
NeilSCGH <neil.segard@gmail.com>
2020-05-01 16:57:10 +0200

    maj

--- Linear Regression/main.py
+++ Linear Regression/main.py
@@ -7,52 +7,72 @@ from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
 from sklearn.metrics import mean_absolute_error
 from sklearn.metrics import r2_score
+from lib.tools import *
 import math
 
-#Model building
 
-##Getting the cleaned data
-df_clean = pd.read_csv('data_cleaned.csv')
+class program():
+	def __init__(self,args):
+	    self.tool = tools(args)
 
-#Loading the target variable Overall into a dataframe and name it y. Then, the features into a second dataframe and name it X. Plot a histogram of y
-targetColumn='Overall'
-y = df_clean[targetColumn] #result column
-X = df_clean.drop(targetColumn, axis=1, inplace=False)
-#y.hist(bins=100)
+		#the column to predict
+	    #self.targetColumn = "Overall"
+	    #self.fileName = "data_cleaned.csv"
+	    self.setup(args)
 
-#Spliting the data set into a training set and a test set
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=123)
-print("train set shape: ", X_train.shape, y_train.shape)
-print("test set shape: ", X_test.shape, y_test.shape)
+	def setup(self,args):
+	    if self.tool.argHasValue("-f"):#the file where to read data
+	      val=self.tool.argValue("-f")
+	      self.fileName=val
+	    else:
+	      self.stop("Error, -f is missing !")
 
-##Training the model
-#Fitting a linear model to the training set
-reg = LinearRegression()
-reg.fit(X_train, y_train)
+	    if self.tool.argHasValue("-o"):#the column to predict
+	      val=self.tool.argValue("-o")
+	      self.targetColumn=val
+	    else:
+	      self.stop("Error, -o is missing !")
 
-train_score = reg.score(X_train, y_train)
-print  ('\ntrain score =', np.round(train_score,6))
+	def run(self):
+		##Getting the cleaned data
+		try:
+			df_clean = pd.read_csv(self.fileName)
+		except:
+			self.stop("Error, file not found !")
 
-##Testing the model
-y_pred = reg.predict(X_test)
-test_score = r2_score(y_test,y_pred)
-#test_score=reg.score(X_test, y_test)
-print  ('test score =', np.round(test_score,6))
+		y = df_clean[self.targetColumn] #result column
+		X = df_clean.drop(self.targetColumn, axis=1, inplace=False)
+		#y.hist(bins=100)
 
-mse = mean_squared_error(y_test, y_pred)
-mae = mean_absolute_error(y_test, y_pred)
-print ('mse = {}, rmse = {} mae = {}'.format(np.round(mse,6),np.round(math.sqrt(mse),6),np.round(mae,6)))
+		#Spliting the data set into a training set and a test set
+		X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=123)
+		print("train set shape: ", X_train.shape, y_train.shape)
+		print("test set shape: ", X_test.shape, y_test.shape)
 
-#Plotting a scatter plot where y_test is in the x axis and  y_pred is in the y axis
-plt.figure(figsize= (10, 5))
-plt.scatter(y_test, y_pred)
-plt.xlabel('y_test')
-plt.ylabel('y_pred')
-plt.show()
+		##Training the model
+		#Fitting a linear model to the training set
+		reg = LinearRegression()
+		reg.fit(X_train, y_train)
 
-#Plotting a histogram of the residuals (difference between y_test and y_pred)
-# plt.figure(figsize= (8, 3))
-# plt.hist(y_test - y_pred)
-# plt.xlabel('residuals')
-# plt.ylabel('')
-# plt.show()
+		train_score = reg.score(X_train, y_train)
+		print  ('\ntrain score =', np.round(train_score,6))
+
+		##Testing the model
+		y_pred = reg.predict(X_test)
+		test_score = r2_score(y_test,y_pred)
+		print  ('test score =', np.round(test_score,6))
+
+		mse = mean_squared_error(y_test, y_pred)
+		mae = mean_absolute_error(y_test, y_pred)
+		print ('mse = {}, rmse = {} mae = {}'.format(np.round(mse,6),np.round(math.sqrt(mse),6),np.round(mae,6)))
+
+		#Plotting a scatter plot where y_test is in the x axis and  y_pred is in the y axis
+		plt.figure(figsize= (10, 5))
+		plt.scatter(y_test, y_pred)
+		plt.xlabel('y_test')
+		plt.ylabel('y_pred')
+		#plt.show()
+
+	def stop(self,msg):
+		print(msg)
+		exit(0)
---
+++ Linear Regression/readme.txt
@@ -0,0 +1,5 @@
+python LR.py -h
+-f + file name
+-o + output column name
+-c clean the file before use
+-d debug
